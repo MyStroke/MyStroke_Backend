9ee44c9f6397677271dce26a56561fe842a1f847
B_K <chayanongg2550@gmail.com>
2023-10-11 02:27:49 +0700

    fix docker

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
-FROM python:3.11.5
+FROM python:3.11
 
 # set working directory
 WORKDIR /app
 
+RUN apt-get update && apt-get install -y python3-opencv
+
 # copy files
 COPY src/mystroke_backend /app/src/mystroke_backend
 COPY pyproject.toml /app/
-COPY hand_efficientnet_91_224 /app/hand_efficientnet_91_224
+COPY "hand_mediapipe_85-95_(21_2)" "/app/hand_mediapipe_85-95_(21_2)"
 COPY README.md /app/
 COPY .python-version /app/
 
@@ -15,6 +17,8 @@ RUN pip3 install .
 
 # run server
 CMD ["uvicorn", "src.mystroke_backend.main:app", "--host", "0.0.0.0", "--port", "5000"]
+
+#CMD ["pip3", "list"]
 
 # bind port
 EXPOSE 5000
--- src/mystroke_backend/main.py
+++ src/mystroke_backend/main.py
@@ -5,10 +5,11 @@ import io
 from fastapi import FastAPI, File
 from fastapi.middleware.cors import CORSMiddleware
 
+import mediapipe as mp
+
 import numpy as np
 import tensorflow as tf
 from PIL import Image
-from tensorflow import keras
 
 
 app = FastAPI()
@@ -23,7 +24,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-loaded_model = tf.saved_model.load("hand_efficientnet_91_224")
+loaded_model = tf.saved_model.load("hand_mediapipe_85-95_(21_2)")
+hands = mp.solutions.hands.Hands()
 
 
 @app.get("/")
@@ -36,10 +38,25 @@ async def predict(image: Annotated[bytes, File()]):
     img = Image.open(io.BytesIO(image))
     img = img.convert("RGB")
 
-    img_array = np.array(img)
-    img_array = tf.image.resize(img_array, (224, 224))
+    wpercent = 512 / img.size[0]
+    hsize = int(img.size[1] * wpercent)
+    img = img.resize((512, hsize), Image.Resampling.LANCZOS)
 
-    prediction, _ = loaded_model(img_array)
+    img = np.array(img)
+
+    hands_result = hands.process(img)
+    hand_landmark = hands_result.multi_hand_landmarks
+
+    if hand_landmark is None:
+        return {"prediction": None}
+
+    landmark_list = []
+    for landmark in hand_landmark[0].landmark:
+        landmark_list.append(np.array([landmark.x, landmark.y]))
+
+    landmark_array = np.array(landmark_list)
+
+    prediction, _ = loaded_model(landmark_array)
     prediction = np.array(prediction[0])
     return {
         "prediction": prediction.tolist(),
